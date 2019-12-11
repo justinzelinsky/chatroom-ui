aeaf6e385d2f6f367f286355d4b0ae1b099c68e9
Justin Zelinsky <jpzelinsky@gmail.com>
2019-12-10 23:11:18 -0500

    Fix authFetch

--- src/utils/authFetch.js
+++ src/utils/authFetch.js
@@ -2,27 +2,23 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
-const request = (url, options) => {
+const doFetch = (url, options) => {
   return new Promise((resolve, reject) => {
     fetch(url, options).then(async response => {
-      try {
-        const body = await response.json();
-        if (response.status >= 200 && response.status < 300) {
-          resolve(body);
-        } else {
-          reject(body);
-        }
-      } catch (err) {
-        reject();
+      const body = await response.json();
+      if (response.ok) {
+        resolve(body);
+      } else {
+        reject(body);
       }
     });
   });
 };
 
-export const get = async url => request(url, { headers, method: 'GET' });
+export const get = async url => doFetch(url, { headers, method: 'GET' });
 
 export const post = async (url, body) =>
-  request(url, { body: JSON.stringify(body), headers, method: 'POST' });
+  doFetch(url, { body: JSON.stringify(body), headers, method: 'POST' });
 
 export const setAuthToken = token => {
   if (token) {
