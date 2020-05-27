ff2f0ed972479b48b3a48741d4977c2ae4f99906
Justin Zelinsky <jpzelinsky@gmail.com>
2020-05-26 23:48:36 -0400

    Sort imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import './styles/globals.scss';
-
 import 'regenerator-runtime/runtime';
+
+import App from 'components/App';
 import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
-import App from 'components/App';
 import store, { history } from 'state/store';
 
 ReactDOM.render(
