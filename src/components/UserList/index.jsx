0b3c2f853019c027cb7ad5ae9576de04362f17d1
Justin Zelinsky <jpzelinsky@gmail.com>
2021-01-20 19:48:54 -0500

    Add dark mode class to user list

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,13 +1,15 @@
 import S from 'components/UserList/styled';
 import UserStatus from 'components/UserStatus';
 import { useSelector } from 'react-redux';
+import useDarkMode from 'state/hooks/useDarkMode';
 import { getUserList } from 'state/selectors';
 
 function UserList () {
   const userList = useSelector(getUserList);
+  const { darkModeClass } = useDarkMode();
 
   return (
-    <S.UserList>
+    <S.UserList className={darkModeClass}>
       {userList.map(({ isActive, isSelf, name }, idx) => (
         <UserStatus
           isActive={isActive}
