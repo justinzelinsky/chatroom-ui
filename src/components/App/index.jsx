f8be8bd50a9856c69ac5d3351866ca32b802fe4e
Justin Zelinsky <jpzelinsky@gmail.com>
2021-01-22 23:54:41 -0500

    Consolidate app component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,10 +4,14 @@ import NotificationBar from 'components/NotificationBar';
 import Register from 'components/Register';
 import { ProtectedRoute, UnprotectedRoute } from 'components/Routes';
 import { LoadingSpinner } from 'components/UI';
-import { Fragment, lazy, Suspense, useEffect } from 'react';
+import { ConnectedRouter } from 'connected-react-router';
+import { lazy, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { Redirect, Switch } from 'react-router-dom';
 import actions from 'state/actions';
+import store, { history } from 'state/store';
+import { GlobalStyle } from 'styles/globals';
 
 const AdminPanel = lazy(() => import('components/AdminPanel'));
 const Chatroom = lazy(() => import('components/Chatroom'));
@@ -21,35 +25,38 @@ function App () {
   }, [dispatch]);
 
   return (
-    <Fragment>
-      <NavigationBar />
-      <NotificationBar />
-      <Suspense fallback={<LoadingSpinner />}>
-        <Switch>
-          <ProtectedRoute
-            component={Chatroom}
-            path="/chatroom"
-          />
-          <ProtectedRoute
-            component={AdminPanel}
-            path="/admin"
-          />
-          <ProtectedRoute
-            component={Profile}
-            path="/profile"
-          />
-          <UnprotectedRoute
-            component={Register}
-            path="/register"
-          />
-          <UnprotectedRoute
-            component={Login}
-            path="/login"
-          />
-          <Redirect to="/login" />
-        </Switch>
-      </Suspense>
-    </Fragment>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <GlobalStyle />
+        <NavigationBar />
+        <NotificationBar />
+        <Suspense fallback={<LoadingSpinner />}>
+          <Switch>
+            <ProtectedRoute
+              component={Chatroom}
+              path="/chatroom"
+            />
+            <ProtectedRoute
+              component={AdminPanel}
+              path="/admin"
+            />
+            <ProtectedRoute
+              component={Profile}
+              path="/profile"
+            />
+            <UnprotectedRoute
+              component={Register}
+              path="/register"
+            />
+            <UnprotectedRoute
+              component={Login}
+              path="/login"
+            />
+            <Redirect to="/login" />
+          </Switch>
+        </Suspense>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,9 @@
 import 'regenerator-runtime/runtime';
 
 import App from 'components/App';
-import { ConnectedRouter } from 'connected-react-router';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store, { history } from 'state/store';
-import { GlobalStyle } from 'styles/globals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <GlobalStyle />
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <App />,
   document.getElementById('app')
 );
