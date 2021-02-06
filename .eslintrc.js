01e75b2868f8774efc216c67da50c77ec657b201
Justin Zelinsky <jpzelinsky@gmail.com>
2021-02-05 21:07:58 -0500

    Eslint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ const ERROR = 2;
 const OFF = 0;
 
 module.exports = {
-  parser: '@babel/eslint-parser',
   env: {
     browser: true,
     es6: true,
@@ -14,45 +13,36 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended'
   ],
-  root: true,
+  globals: {
+    API_ADDRESS: true
+  },
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 2021,
     sourceType: 'module'
   },
-  globals: {
-    API_ADDRESS: true
-  },
   plugins: [
-    'react',
     'jest',
+    'react',
     'simple-import-sort'
   ],
   rules: {
+    'indent': [ERROR, 2, { SwitchCase: 1 }],
+    'keyword-spacing': ERROR,
+    'linebreak-style': [ERROR, 'unix'],
+    'no-trailing-spaces': ERROR,
+    'object-curly-spacing': [ERROR, 'always'],
+    'quotes': [ERROR, 'single'],
+    'react/react-in-jsx-scope': OFF,
+    'react/jsx-indent-props': [ERROR, 'first'],
+    'semi': [ERROR, 'always'],
     'simple-import-sort/imports': ERROR,
     'space-before-function-paren': ['error', 'always'],
-    'keyword-spacing': ERROR,
-    'no-trailing-spaces': ERROR,
-    indent: [ERROR, 2, { SwitchCase: 1 }],
-    'linebreak-style': [ERROR, 'unix'],
-    quotes: [ERROR, 'single'],
-    semi: [ERROR, 'always'],
-    'react/jsx-indent-props': [ERROR, 'first'],
-    'object-curly-spacing': [ERROR, 'always'],
-    'no-console': OFF,
-    'sort-imports': OFF,
-    'import/order': OFF,
-    'react/jsx-uses-react': OFF,
-    'react/react-in-jsx-scope': OFF
   },
   settings: {
-    react: {
-      createClass: 'createReactClass',
-      pragma: 'React',
-      version: '17.0.1'
-    },
     propWrapperFunctions: ['forbidExtraProps']
   }
 };
