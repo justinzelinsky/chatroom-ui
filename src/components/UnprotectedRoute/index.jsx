5ef3fe05bc5efd06b38dfe0002ea23823cfa047a
Justin Zelinsky <jpzelinsky@gmail.com>
2021-03-05 23:36:53 -0500

    Fix missing import

--- src/components/UnprotectedRoute/index.jsx
+++ src/components/UnprotectedRoute/index.jsx
@@ -1,6 +1,6 @@
 import { elementType } from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
-
+import useCurrentUser from 'state/hooks/useCurrentUser';
 function UnprotectedRoute ({ component: Component, ...rest }) {
   const { isAuthenticated } = useCurrentUser();
 
